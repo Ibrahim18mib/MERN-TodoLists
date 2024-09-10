0a6144c1ea4ca242ee110a952e9611cecc6ab11c
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2024-09-10 01:30:50 -1200

    implemented and integrated edit,update and delete apis

--- todo-front/src/Todo.js
+++ todo-front/src/Todo.js
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Todo() {
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [editTitle, setEditTitle] = useState("");
+    const [editDescription, setEditDescription] = useState("");
     const [todos, setTodos] = useState([]);
+    const [editId, setEditId] = useState(-1);
     //res message,error state
     const [message, setMsg] = useState("");
     const [error, setErr] = useState("");
@@ -13,6 +16,7 @@ export default function Todo() {
 
     const handleSubmit = () => {
         console.log("Submit clicked");
+        setErr("");
         if (title.trim() !== '' && description.trim() !== '') {
             //pass tot he api
             fetch(apiUrl + '/todos', {
@@ -26,10 +30,17 @@ export default function Todo() {
                     debugger
                     //add in the todos array
                     setTodos([...todos, { title, description }])
-                    setMsg("Item added Successfully")
+                    setTitle("");
+                    setDescription("");
+                    setMsg("Item added Successfully");
+                    setTimeout(() => {
+                        setMsg("");
+                    }, 2000);
                 } else {
                     setErr("Error while creating in DB")
                 }
+            }).catch(() => {
+                setErr("Unable to create todo lists");
             })
 
 
@@ -37,6 +48,81 @@ export default function Todo() {
         }
     }
 
+    useEffect(() => {
+        getAllItems();
+    }, [])
+
+    const getAllItems = () => {
+        fetch(apiUrl + "/todos").then((res1) => res1.json()).then((res2) => {
+            setTodos(res2);
+        })
+    }
+
+    const handleEdit = (item) => {
+        setEditId(item._id);
+        setEditTitle(item.title);
+        setEditDescription(item.description);
+
+    }
+
+    const handleUpdate = () => {
+        console.log("Update clicked");
+        setErr("");
+        debugger
+        if (editTitle.trim() !== '' && editDescription.trim() !== '') {
+            //pass tot he api
+            fetch(apiUrl + '/todos/' + editId, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title: editTitle, description: editDescription })
+            }).then((res) => {
+                if (res.ok) {
+                    debugger
+                    //update in the todos array
+                    const updatedTodos = todos.map((item) => {
+                        if (item._id === editId) {
+                            item.title = editTitle;
+                            item.description = editDescription;
+                        }
+                        return item;
+                    })
+                    setTodos(updatedTodos);
+                    setEditTitle("");
+                    setDescription("");
+                    setMsg("Item Updated Successfully");
+                    setTimeout(() => {
+                        setMsg("");
+                    }, 2000);
+                    setEditId(-1);
+                } else {
+                    setErr("Error while Updating in DB")
+                }
+            }).catch(() => {
+                setErr("Unable to Update todo lists");
+            })
+
+
+
+        }
+    }
+
+    const handleDelete = (id) => {
+        if (window.confirm("are you sure to delete?")) {
+            fetch(apiUrl + "/todos/" + id, {
+                method: "DELETE"
+            }).then(() => {
+                const updatedTodos = todos.filter((item) => item._id !== id);
+                setTodos(updatedTodos);
+            })
+        }
+
+    }
+
+    const handleEditCancel = () => {
+        setEditId(-1);
+    }
 
     return <>
         <div className="row p-3 bg-success text-light">
@@ -51,6 +137,45 @@ export default function Todo() {
                 <button className="btn btn-dark" onClick={handleSubmit}>Submit</button>
             </div>
             {error && <p className='text-danger'>{error}</p>}
+        </div>
+        <div className='row mt-3'>
+            <h2>Tasks</h2>
+            <div className='col-md-6'>
+                <ul className='list-group'>
+                    {
+                        todos.map((item) =>
+
+                            <li className='list-group-item bg-info d-flex justify-content-between align-items-center my-2'>
+                                <div className='d-flex flex-column gap-2 me-2'>
+                                    {
+                                        editId === -1 || editId !== item._id ?
+                                            <>
+                                                <span className='fw-bold'>{item?.title}</span>
+                                                <span>{item?.description}</span>
+                                            </> :
+                                            <>
+                                                <div className="form-group d-flex gap-2">
+                                                    <input type="text" onChange={(event) => setEditTitle(event.target.value)} value={editTitle} className="form-control" placeholder="Title" />
+                                                    <input type="text" onChange={(event) => setEditDescription(event.target.value)} value={editDescription} className="form-control" placeholder="Description" />
+                                                </div>
+                                            </>
+                                    }
+
+                                </div>
+
+                                <div className='d-flex gap-2'>
+                                    {editId === -1 || editId !== item._id ? <button className='btn btn-primary' onClick={() => handleEdit(item)}>Edit</button> :
+                                        <button className='btn btn-secondary' onClick={handleUpdate}>Update</button>}
+                                    {editId === -1 || editId !== item._id ? <button className='btn btn-warning' onClick={() => handleDelete(item._id)}>Delete</button> : <button className='btn btn-danger' onClick={handleEditCancel}>Cancel</button>}
+
+                                </div>
+                            </li>
+                        )
+                    }
+
+                </ul>
+            </div>
+
         </div>
     </>
 }
